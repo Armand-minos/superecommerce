076d299750521e51622a954c8ff4d05655a29221
Armand <armand.moisan08@gmail.com>
2025-02-07 21:09:29 +0100

    dd

---
+++ onglet.php
@@ -0,0 +1,9 @@
+<div class="tabs">
+                <div class="tab-labels">
+                  <span data-id="1" class="active">Info</span>
+                  <span data-id="2">Brand</span>
+                </div>
+                <div class="tab-slides">
+                  <div id="tab-slide-1" itemprop="description"  class="slide active">
+                    We open source it for you https://github.com/greenwoodents/quickbeam.js if you want to use it on your ecommerce.
+                  </div>
